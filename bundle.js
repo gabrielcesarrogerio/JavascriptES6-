7f33c0bef5d87aecfcb89cdd8bc430e88d526f2b
Gabbss <gabespeedy@live.com>
2020-07-17 19:20:35 -0300

    enter

--- bundle.js
+++ bundle.js
@@ -1,83 +1,67 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var usuario = /*#__PURE__*/function () {
-  function usuario() {
-    _classCallCheck(this, usuario);
-
-    this.email = [];
-    this.senha = [];
-    this.admin = false;
+//EXERCÍCIO 1
+// class usuario{
+//     constructor (){
+//         this.email = []
+//         this.senha = [];
+//         this.admin = false;
+//     }
+//         add(email, senha){
+//             this.email.push(email);
+//             this.senha.push(senha); 
+//         }
+//         isadmin(){
+//             if(this.admin==false){
+//                 console.log('false');
+//             }else{
+//                 console.log('true');
+//             }
+//         }        
+// }
+// class admin extends usuario{
+//     constructor(){
+//         super();
+//         this.admin = true;
+//     }    
+// }
+// const user1 = new usuario ();
+// user1.add('email@teste.com', 'senha123');
+// const adm1 = new admin ();
+// adm1.add('email@teste.com', 'senha123');
+// console.log(user1.isadmin());
+// console.log(adm1.isadmin())
+// console.log(user1);
+// console.log(adm1);
+//EXERCÍCIO 2
+var usuarios = [{
+  nome: 'Diego',
+  idade: '23',
+  empresa: 'Rocketseat'
+}, {
+  nome: 'Gabriel',
+  idade: '15',
+  empresa: 'Rocketseat'
+}, {
+  nome: 'Lucas',
+  idade: '30',
+  empresa: 'Facebook'
+}];
+var newarr = usuarios.map(function (item) {
+  return item.idade;
+});
+console.log(newarr);
+var secarr = usuarios.filter(function (item) {
+  return item.idade >= '18' && item.empresa == 'Rocketseat';
+});
+console.log(secarr);
+var terarr = usuarios.find(function (item) {
+  return item.empresa == 'Google';
+});
+console.log(terarr);
+var qrarr = usuarios.map(function (item) {
+  if (item.idade <= '50') {
+    return item;
   }
-
-  _createClass(usuario, [{
-    key: "add",
-    value: function add(email, senha) {
-      this.email.push(email);
-      this.senha.push(senha);
-    }
-  }, {
-    key: "isadmin",
-    value: function isadmin() {
-      if (this.admin == false) {
-        console.log('false');
-      } else {
-        console.log('true');
-      }
-    }
-  }]);
-
-  return usuario;
-}();
-
-var admin = /*#__PURE__*/function (_usuario) {
-  _inherits(admin, _usuario);
-
-  var _super = _createSuper(admin);
-
-  function admin() {
-    var _this;
-
-    _classCallCheck(this, admin);
-
-    _this = _super.call(this);
-    _this.admin = true;
-    return _this;
-  }
-
-  _createClass(admin, [{
-    key: "addadmin",
-    value: function addadmin(emailadmin, senhaadmin) {
-      this.email.push(emailadmin);
-      this.senha.push(senhaadmin);
-    }
-  }]);
-
-  return admin;
-}(usuario);
-
-var user1 = new usuario('email@teste.com', 'senha123');
-var adm1 = new admin('email@teste.com', 'senha123');
-console.log(user1.isadmin());
-console.log(adm1.isadmin());
+});
+console.log(qrarr);
--- maindesafioum.js
+++ maindesafioum.js
@@ -1,40 +1,60 @@
-class usuario{
-    constructor (){
-        this.email = [];
-        this.senha = [];
-        this.admin = false;
-    }
-        add(email, senha){
-            this.email.push(email);
-            this.senha.push(senha);
-            
-        }
-        isadmin(){
-            if(this.admin==false){
-                console.log('false');
-            }else{
-                console.log('true');
-            }
-        }
-        
-}
+//EXERCÍCIO 1
 
-class admin extends usuario{
-    constructor(){
-        super();
-        this.admin = true;
-    }
-    addadmin(emailadmin, senhaadmin){
-        this.email.push(emailadmin);
-        this.senha.push(senhaadmin);
-        
-    }
-    
+// class usuario{
+//     constructor (){
+//         this.email = []
+//         this.senha = [];
+//         this.admin = false;
+//     }
+//         add(email, senha){
+//             this.email.push(email);
+//             this.senha.push(senha); 
+//         }
+//         isadmin(){
+//             if(this.admin==false){
+//                 console.log('false');
+//             }else{
+//                 console.log('true');
+//             }
+//         }        
+// }
 
-}
+// class admin extends usuario{
+//     constructor(){
+//         super();
+//         this.admin = true;
+//     }    
+// }
 
-const user1 = new usuario ('email@teste.com', 'senha123');
-const adm1 = new admin ('email@teste.com', 'senha123');
+// const user1 = new usuario ();
+// user1.add('email@teste.com', 'senha123');
+// const adm1 = new admin ();
+// adm1.add('email@teste.com', 'senha123');
+// console.log(user1.isadmin());
+// console.log(adm1.isadmin())
+// console.log(user1);
+// console.log(adm1);
 
-console.log(user1.isadmin());
-console.log(adm1.isadmin())
+//EXERCÍCIO 2
+
+const usuarios = [
+    { nome : 'Diego', idade : '23', empresa : 'Rocketseat'},
+    { nome : 'Gabriel', idade : '15', empresa : 'Rocketseat'},
+    { nome : 'Lucas', idade : '30', empresa : 'Facebook'},
+]
+
+const newarr = usuarios.map(item => item.idade);
+console.log(newarr);
+
+const secarr = usuarios.filter(item => item.idade >= '18' && item.empresa == 'Rocketseat');
+console.log(secarr);
+
+const terarr = usuarios.find(item => item.empresa == 'Google');
+console.log(terarr);
+
+const qrarr = usuarios.map(function(item){    
+        if(item.idade <= '50'){
+            return item;
+        }    
+});
+console.log(qrarr);
