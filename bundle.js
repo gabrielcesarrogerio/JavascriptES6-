8a8b7aa8b5f880561b76709110ea6af259125dd1
Gabbss <gabespeedy@live.com>
2020-07-20 17:22:15 -0300

    enter

--- bundle.js
+++ bundle.js
@@ -1,5 +1,3 @@
-"use strict";
-
 //EXERCÍCIO 1
 // class usuario{
 //     constructor (){
@@ -34,34 +32,42 @@
 // console.log(user1);
 // console.log(adm1);
 //EXERCÍCIO 2
-var usuarios = [{
-  nome: 'Diego',
-  idade: '23',
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Gabriel',
-  idade: '15',
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Lucas',
-  idade: '30',
-  empresa: 'Facebook'
-}];
-var newarr = usuarios.map(function (item) {
-  return item.idade;
-});
-console.log(newarr);
-var secarr = usuarios.filter(function (item) {
-  return item.idade >= '18' && item.empresa == 'Rocketseat';
-});
-console.log(secarr);
-var terarr = usuarios.find(function (item) {
-  return item.empresa == 'Google';
-});
-console.log(terarr);
-var qrarr = usuarios.map(function (item) {
-  if (item.idade <= '50') {
-    return item;
-  }
-});
-console.log(qrarr);
+// const usuarios = [
+//     { nome : 'Diego', idade : '23', empresa : 'Rocketseat'},
+//     { nome : 'Gabriel', idade : '15', empresa : 'Rocketseat'},
+//     { nome : 'Lucas', idade : '30', empresa : 'Facebook'},
+// ]
+// const newarr = usuarios.map(item => item.idade);
+// console.log(newarr);
+// const secarr = usuarios.filter(item => item.idade >= '18' && item.empresa == 'Rocketseat');
+// console.log(secarr);
+// const terarr = usuarios.find(item => item.empresa == 'Google');
+// console.log(terarr);
+// const qrarr = usuarios.map(function(item){
+//         const newit = item.idade * 2;
+//         item.idade = newit;
+//         if (item.idade <= 50){
+//             return item;
+//         }
+// });
+// console.log(qrarr);
+//EXERCICIO 3
+//3.1
+// const arr = [1,2,3,4,5];
+// const arrtest = arr.map(item => item +10);
+// console.log(arrtest);
+//3.2
+// const usuario = {nome:'Diego', idade:23};
+// const mostraidade = (usuario) => usuario.idade;
+// console.log(mostraidade(usuario));
+//3.3
+// const nome = "Diego";
+// const idade = 23;
+// const mostrausuario = (nome = 'Diego', idade = 18) => ({nome, idade});
+// console.log(mostrausuario(nome,idade));
+// console.log(mostrausuario(nome));
+//3.4
+// const promise = () => new Promise(resolve, reject => resolve);
+// console.log(promise);
+//EXERCÍCIO 4
+"use strict";
--- maindesafioum.js
+++ maindesafioum.js
@@ -37,24 +37,57 @@
 
 //EXERCÍCIO 2
 
-const usuarios = [
-    { nome : 'Diego', idade : '23', empresa : 'Rocketseat'},
-    { nome : 'Gabriel', idade : '15', empresa : 'Rocketseat'},
-    { nome : 'Lucas', idade : '30', empresa : 'Facebook'},
-]
+// const usuarios = [
+//     { nome : 'Diego', idade : '23', empresa : 'Rocketseat'},
+//     { nome : 'Gabriel', idade : '15', empresa : 'Rocketseat'},
+//     { nome : 'Lucas', idade : '30', empresa : 'Facebook'},
+// ]
 
-const newarr = usuarios.map(item => item.idade);
-console.log(newarr);
+// const newarr = usuarios.map(item => item.idade);
+// console.log(newarr);
 
-const secarr = usuarios.filter(item => item.idade >= '18' && item.empresa == 'Rocketseat');
-console.log(secarr);
+// const secarr = usuarios.filter(item => item.idade >= '18' && item.empresa == 'Rocketseat');
+// console.log(secarr);
 
-const terarr = usuarios.find(item => item.empresa == 'Google');
-console.log(terarr);
+// const terarr = usuarios.find(item => item.empresa == 'Google');
+// console.log(terarr);
 
-const qrarr = usuarios.map(function(item){    
-        if(item.idade <= '50'){
-            return item;
-        }    
-});
-console.log(qrarr);
+// const qrarr = usuarios.map(function(item){
+//         const newit = item.idade * 2;
+//         item.idade = newit;
+//         if (item.idade <= 50){
+//             return item;
+//         }
+// });
+// console.log(qrarr);
+
+//EXERCICIO 3
+
+//3.1
+
+// const arr = [1,2,3,4,5];
+// const arrtest = arr.map(item => item +10);
+// console.log(arrtest);
+
+//3.2
+
+// const usuario = {nome:'Diego', idade:23};
+// const mostraidade = (usuario) => usuario.idade;
+// console.log(mostraidade(usuario));
+
+//3.3
+
+// const nome = "Diego";
+// const idade = 23;
+
+// const mostrausuario = (nome = 'Diego', idade = 18) => ({nome, idade});
+// console.log(mostrausuario(nome,idade));
+// console.log(mostrausuario(nome));
+
+//3.4
+
+// const promise = () => new Promise(resolve, reject => resolve);
+// console.log(promise);
+
+//EXERCÍCIO 4
+
